2049677f3c86101d78b26138a916a89cb6770a8d
Abir Khan <abirkhan86160@gmail.com>
2026-04-17 19:19:39 +0600

    modiul 7 class 1

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # All-js-practics-repo
+----------------------------------------------
+varible
+--------------------
+data type-
+
+data type off varible
+string
+number
+boolean
+undefind
+null
+object
+array
+reg exp
+----------------------
+operator
+
+         var mark = 85;
+              if(mark<=100 && mark>=80){
+                console.log('A+')
+              }
+              else if(mark<80 && mark>=70){
+                console.log('A')
+              }
+              else if(mark<70 && mark>=60){
+                console.log('A-')
+              }
+              else if(mark<60 && mark>=50){
+                console.log('B')
+              }
+              else if(mark<50 && mark>=40){
+                console.log('C')
+              }
+              else if(mark<40 && mark>=33){
+                console.log('D')
+              }
+              else{
+                console.log('F') 
+              }
+
+
+
+              
---
+++ index.js
@@ -0,0 +1,52 @@
+console.log('Hello, World!');
+let num1 =10;
+        let num2 =20;
+        let sum = num1+num2;
+        console.log(sum)
+
+
+
+   var person = {
+            name: 'Abir Khan',
+            age: 25,
+            isStudent: true
+         };     
+            console.log(person)//object 
+
+
+
+              var city=[
+                'Dhaka',
+                'Chittagong',
+                'Khulna',
+                'Rajshahi',
+                'Barishal'
+              ];
+              console.log(city)//array of string
+
+
+              var mark = 9;
+              if (mark<=100 && mark>=80) {
+                console.log('A+');
+              }
+
+              else if (mark<=80 && mark>=70) {
+                console.log('A-');
+              }
+              else if (mark<=70 && mark>=60) {
+                console.log('A');
+              }
+              else if (mark<=60 && mark>=50) {
+                console.log('B');
+              }
+              else if (mark<=50 && mark>=40) {
+                console.log('C');
+              }
+              else if (mark<=40 && mark>=33) {
+                console.log('D');
+              }
+
+              else {
+                console.log('Fail');
+              }
+             
